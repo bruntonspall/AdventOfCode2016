f8758644b08a9e0034b5a572bd51a9c270fca030
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2016-12-04 13:40:35 +0000

    Use pythons Counter

--- day04/rooms.py
+++ day04/rooms.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def cmp_by_number_then_alphabet(x,y):
     if x[1] == y[1]:
         return cmp(x[0],y[0])
@@ -5,23 +7,15 @@ def cmp_by_number_then_alphabet(x,y):
         return cmp(y[1], x[1])
 
 def calculate_checksum(s):
-    s = sorted(s.replace("-","")[:-3])
-    d = {}
-    for character in s:
-        if character in d:
-            d[character] += 1
-        else:
-            d[character] = 1
-    return "".join([letter for letter,number in sorted(d.items(),cmp=cmp_by_number_then_alphabet)][0:5])
+    c = Counter(s.replace("-","")[:-3])
+    counts = sorted(c.most_common(), cmp=cmp_by_number_then_alphabet)[:5]
+    return "".join([letter for letter,number in counts])
 
 def sum_from_lines(f):
     total = 0
     for line in f:
         room = line[:-7]
-        checksum = line[-6:-1]
-        if calculate_checksum(room) == checksum:
+        if calculate_checksum(room) == line[-6:-1]:
             total += int(room[-3:])
-        else:
-            pass
     return total
 
