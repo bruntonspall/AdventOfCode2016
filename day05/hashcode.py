43d96b1d4c5442ab1b6991543dafc14c8db080e3
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2016-12-15 21:00:32 +0000

    Updated hash function, still 30s to run though

--- day05/hashcode.py
+++ day05/hashcode.py
@@ -3,26 +3,37 @@ import hashlib
 def valid_hash(code):
     h = hashlib.md5(code).hexdigest()
     if h[0:5] == "00000":
-        return h[5]
+        return h[5],h[6]
     else:
-        return ""
+        return "",""
 
 def find_hash(code, start):
     found = False
     h = ""
     while not found:
-        h = valid_hash(code+str(start))
+        h,h2 = valid_hash(code+str(start))
         if h:
             found = True
         else:
             start += 1
-    return h, start
+    return h, start, h2
 
 def find_code(code):
     password = ""
     start = 0
     while len(password) < 8:
-        h, start = find_hash(code, start)
+        h, start, h2 = find_hash(code, start)
         password += h
+        start += 1
+    return password
+
+def find_code2(code):
+    password = [" "," "," "," "," "," "," "," "]
+    start = 0
+    while " " in password:
+        h, start, h2 = find_hash(code, start)
+        if h.isdigit() and int(h) < 8:
+            if password[int(h)] == " ":
+                password[int(h)] = h2
         start += 1
     return password
--- day05/test_hashcode.py
+++ day05/test_hashcode.py
@@ -2,13 +2,16 @@ import unittest
 from hashcode import *
 
 class RoomTest(unittest.TestCase):
-    def test_simple_hash(self):
-        self.assertEquals(valid_hash("abc3231928"), "")
-        self.assertEquals(valid_hash("abc3231929"), "1")
+    # def test_simple_hash(self):
+    #     self.assertEquals(valid_hash("abc3231928"), ("", ""))
+    #     self.assertEquals(valid_hash("abc3231929"), ("1", "5"))
+    #
+    # def test_find_hash(self):
+    #     self.assertEquals(find_hash("abc", 0), ("1", 3231929, "5"))
+        # self.assertEquals(find_hash("abc", 3231930), ("8", 5017308, "f"))
 
-    def test_find_hash(self):
-        self.assertEquals(find_hash("abc", 0), ("1", 3231929))
-        self.assertEquals(find_hash("abc", 3231930), ("8", 5017308))
+    # def test_8digitcode(self):
+    #     self.assertEquals(find_code("abc"), "18f47a30")
 
-    def test_8digitcode(self):
-        self.assertEquals(find_code("abc"), "18f47a30")
+    def test_second8digitcode(self):
+        self.assertEquals("".join(find_code2("abc")), "05ace8e3")
