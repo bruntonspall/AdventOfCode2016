77dcb770fcf1a21ff222e44cbe4efb32b514a7cc
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2016-12-20 20:58:40 +0000

    Add SSL test

--- day07/ipv7.py
+++ day07/ipv7.py
@@ -30,6 +30,12 @@ def is_abba(word):
            word[0] != word[1]
 
 
+def is_aba(word):
+    return (len(word) == 3) and \
+           is_palindrome(word) and \
+           word[0] != word[1]
+
+
 def contains_abba(word):
     word_in_fours = [word[n:n+4] for n in range(len(word)) if n+4 <= len(word)]
     for word in word_in_fours:
@@ -43,3 +49,36 @@ def is_valid_addr(addr):
     if filter(contains_abba, hypernets):
         return False
     return len(filter(contains_abba, addrs)) > 0
+
+
+def extract_aba(word):
+    threes = [word[n:n+3] for n in range(len(word)) if n+3 <= len(word)]
+    return filter(is_aba, threes)
+
+
+def flatmap(fn, l):
+    return reduce(list.__add__, map(fn, l))
+
+
+def extract_aba_from_addr(addr):
+    addrs, hypernets = parse_address(addr)
+    return flatmap(extract_aba, addrs), flatmap(extract_aba, hypernets)
+
+
+def convert_aba_to_bab(aba):
+    return aba[1]+aba[0]+aba[1]
+
+
+def supports_ssl(addr):
+    aba_addrs, aba_hypernets = extract_aba_from_addr(addr)
+    for aba in aba_hypernets:
+        bab = convert_aba_to_bab(aba)
+        if bab in aba_addrs:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    f = file("input.txt")
+    lines = [l.strip() for l in f.readlines()]
+    print len([l for l in lines if supports_ssl(l)])
--- day07/test_ipv7.py
+++ day07/test_ipv7.py
@@ -1,5 +1,7 @@
 import unittest
-from ipv7 import parse_address, is_palindrome, contains_abba, is_valid_addr
+from ipv7 import parse_address, is_palindrome, contains_abba, \
+                 is_valid_addr, extract_aba_from_addr, extract_aba, \
+                 supports_ssl
 
 
 class TestIPv7(unittest.TestCase):
@@ -35,3 +37,26 @@ class TestIPv7(unittest.TestCase):
         self.assertTrue(is_valid_addr("fgabcatw[mnop]qasdrst[uvwx]iabbae"))
         self.assertFalse(is_valid_addr("abcd[zxcv]bnmkjgfdfj[bddb]xyyx"))
         self.assertFalse(is_valid_addr("abcd[bddb]bnmkjgfdfj[zxcv]xyyx"))
+
+    def test_extract_aba(self):
+        self.assertEquals(["zaz", "zbz"],
+                          extract_aba("zazbz"))
+
+    def test_contains_aba(self):
+        self.assertEquals((["aba"], ["bab"]),
+                          extract_aba_from_addr("aba[bab]xyz"))
+        self.assertEquals((["xyx", "xyx"], ["xyx"]),
+                          extract_aba_from_addr("xyx[xyx]xyx"))
+        self.assertEquals((["eke"], ["kek"]),
+                          extract_aba_from_addr("aaa[kek]eke"))
+        self.assertEquals((["zaz", "zbz"], ["bzb"]),
+                          extract_aba_from_addr("zazbz[bzb]cdb"))
+
+    def test_supports_ssl(self):
+        self.assertTrue(supports_ssl("aba[bab]xyz"))
+        self.assertFalse(supports_ssl("xyx[xyx]xyx"))
+        self.assertTrue(supports_ssl("aaa[kek]eke"))
+        self.assertTrue(supports_ssl("zazbz[bzb]cdb"))
+        self.assertTrue(supports_ssl("abcd[bdb]bnmkjgfdfj[zxcv]xdbd"))
+        self.assertTrue(supports_ssl("abcd[bdb]bnmkdbddfj[zxcv]xddd"))
+        self.assertTrue(supports_ssl("abcd[bdd]bnmkjgfdfj[bdbv]xdbd"))
